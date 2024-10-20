fc7aa52047653895b5823969ac244cde232b91f9
manu <manuel.nto.rmo@gmail.com>
2024-10-20 23:05:40 +0200

    Alias implemented totaly

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 mod modules {
     pub mod input;
     pub mod command;
+    pub mod alias;
 }
+
+use modules::{alias::AliasManager, command::execute_command};
 
 
 fn main() {
+    let mut alias_manager = AliasManager::new();
+
     loop {
         let input = modules::input::read_input();
 
@@ -20,6 +25,21 @@ fn main() {
         let command = tokens[0];
         let args = &tokens[1..];
 
-        modules::command::execute_command(command, args);
+        if command == "alias" {
+            if args.len() != 2 {
+                eprintln!("Usage: alias <name> <command>");
+            } else {
+                alias_manager.add_alias(args[0], args[1]);
+            }
+            continue;
+        }
+
+        let resolved_command = alias_manager
+            .resolve_alias(command)
+            .cloned()
+            .unwrap_or_else(|| command.to_string());
+
+        execute_command(&resolved_command, args);
+
     }
 }
